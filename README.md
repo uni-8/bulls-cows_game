cc461f26040743574c3b329e7cf99264f29b75ee
uni-8 <julia.zhol.a@gmail.com>
2020-08-30 20:52:36 +0300

    beta-1.0

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Bulls & Cows
+    - [DEMO LINK](https://uni-8.github.io/bulls-cows_game/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,188 @@
 'use strict';
+
+const bgShadow = document.querySelector('.bg-shadow');
+const gameBox = document.querySelector('.container');
+const gameField = document.querySelector('.main-content');
+const button = document.querySelector('.footer__start');
+const buttonDescription = button.innerHTML;
+
+let secretCode;
+let attemptCount = 0;
+let gameStarted = false;
+
+document.addEventListener('keydown', checkKey);
+button.addEventListener('click', startGame);
+
+function checkKey(keyboardEvent) {
+  if (keyboardEvent.key !== 'Enter') {
+    return;
+  }
+
+  if (gameStarted) {
+    addResult();
+
+    return;
+  }
+
+  startGame();
+}
+
+function startGame() {
+  setTimeout(changeField, 1000);
+
+  gameBox.classList.add('hidden');
+  bgShadow.classList.add('hidden');
+
+  if (document.querySelector('.success')) {
+    document.querySelector('.success').remove();
+  }
+
+  function changeField() {
+    secretCode = Math.random().toString().slice(2, 6);
+
+    // eslint-disable-next-line no-console
+    console.log(secretCode);
+
+    button.innerHTML = buttonDescription;
+    gameField.innerHTML = '';
+
+    gameField.insertAdjacentHTML('afterbegin', `
+    <div class="main-content__guess">
+      <label>
+        Enter your guess here:
+        <input type="text" class="main-content__input">
+      </label>
+      <p>
+        Your previous guesses:
+      </p>
+    </div>
+    <div class="main-content__storage">
+    </div>
+  `);
+
+    gameBox.classList.remove('hidden');
+    gameField.querySelector('.main-content__input').focus();
+
+    gameStarted = true;
+
+    button.removeEventListener('click', startGame);
+    button.addEventListener('click', addResult);
+  }
+}
+
+function addResult() {
+  const storage = gameField.querySelector('.main-content__storage');
+  const input = gameField.querySelector('.main-content__input');
+
+  input.focus();
+
+  if (!validateInput(input.value)) {
+    input.value = '';
+
+    return;
+  }
+
+  const result = getHint(secretCode, input.value);
+
+  storage.insertAdjacentHTML('beforeend', `
+      ${result}
+      <br>
+    `);
+
+  storage.scrollTop = storage.scrollHeight;
+  input.value = '';
+
+  function validateInput(value) {
+    switch (true) {
+      case !value:
+      case isNaN(+value):
+      case value.length !== 4:
+        pushNotification(
+          'Wrong input!',
+          'Enter 4 digits',
+          'error'
+        );
+
+        return false;
+
+      default:
+        return true;
+    }
+  }
+
+  function getHint(secret, guess) {
+    const chars = new Map();
+    let bulls = 0;
+    let cows = 0;
+
+    attemptCount++;
+
+    for (let i = 0; i < guess.length; i++) {
+      if (guess[i] === secret[i]) {
+        bulls++;
+      } else {
+        chars.set(secret[i], chars.get(secret[i]) + 1 || 1);
+      }
+    }
+
+    for (let i = 0; i < guess.length; i++) {
+      if (chars.get(guess[i]) > 0 && secret[i] !== guess[i]) {
+        cows++;
+        chars.set(guess[i], chars.get(guess[i]) - 1);
+      }
+    }
+
+    if (bulls === 4) {
+      congratulate();
+    }
+
+    return `Attempt #<span class="main-content__fail">${attemptCount}</span>:
+      <span class="main-content__attempt">${guess}</span>, you guessed
+      <span class="main-content__right">${bulls} bulls</span>,
+      <span class="main-content__almost">${cows} cows</span>
+    `;
+  }
+
+  function congratulate() {
+    const firstW = (attemptCount === 1) ? 'Amazing' : 'Congrats';
+    const lastW = (attemptCount === 1) ? 'try' : 'tries';
+
+    gameField.innerHTML = '';
+    bgShadow.classList.remove('hidden');
+
+    pushNotification(
+      `You won!`,
+      `${firstW}, you guessed all the bulls in ${attemptCount} ${lastW}!`,
+      `success`
+    );
+
+    attemptCount = 0;
+    gameStarted = false;
+
+    button.innerHTML = 'Play again';
+    button.removeEventListener('click', addResult);
+    button.addEventListener('click', startGame);
+  }
+}
+
+function pushNotification(title, description, type) {
+  const message = document.createElement('div');
+
+  message.className = `main-content__notification`;
+  message.classList.add(`main-content__notification--${type}`);
+
+  message.insertAdjacentHTML('afterbegin', `
+    <h2>
+      ${title}
+    </h2>
+    <p>
+      ${description}
+    </p>
+  `);
+
+  document.body.append(message);
+
+  if (type === 'error') {
+    setTimeout(() => message.remove(), 2000);
+  }
+}
